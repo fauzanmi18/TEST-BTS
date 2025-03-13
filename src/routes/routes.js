662b82e18fb233b36c173e945d173d8d5e70f8ba
fauzanmi18 <iqbal.darussalam17@gmail.com>
2025-03-13 13:15:03 +0700

    add middleware auth

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import userController from '../application/user/user.controller.js'
 import todoController from '../application/todo/todo.controller.js'
+import { authMiddleware } from '../middleware/auth.middleware.js'
 
 export const appRoute = new express.Router()
 
@@ -10,14 +11,14 @@ appRoute.post('/login', userController.login)
 appRoute.post('/logout', userController.logout)
 
 // Checklist
-appRoute.get('/checklist', todoController.getAllChecklists)
-appRoute.post('/checklist', todoController.createChecklist)
-appRoute.delete('/checklist', todoController.deleteChecklist)
+appRoute.get('/checklist', authMiddleware, todoController.getAllChecklists)
+appRoute.post('/checklist', authMiddleware, todoController.createChecklist)
+appRoute.delete('/checklist', authMiddleware, todoController.deleteChecklist)
 
 // Checklist item
-appRoute.get('/checklist/:checklistId/item', todoController.getChecklistItems)
-appRoute.post('/checklist/:checklistId/item', todoController.createChecklistItem)
-appRoute.get('/checklist/:checklistId/item/:checklistItemId', todoController.getChecklistItemById)
-appRoute.patch('/checklist/:checklistId/item/:checklistItemId', todoController.updateChecklistItemStatus)
-appRoute.delete('/checklist/:checklistId/item/:checklistItemId', todoController.deleteChecklistItem)
-appRoute.patch('/checklist/:checklistId/item/rename/:checklistItemId', todoController.renameChecklistItem)
+appRoute.get('/checklist/:checklistId/item', authMiddleware, todoController.getChecklistItems)
+appRoute.post('/checklist/:checklistId/item', authMiddleware, todoController.createChecklistItem)
+appRoute.get('/checklist/:checklistId/item/:checklistItemId', authMiddleware, todoController.getChecklistItemById)
+appRoute.patch('/checklist/:checklistId/item/:checklistItemId', authMiddleware, todoController.updateChecklistItemStatus)
+appRoute.delete('/checklist/:checklistId/item/:checklistItemId', authMiddleware, todoController.deleteChecklistItem)
+appRoute.patch('/checklist/:checklistId/item/rename/:checklistItemId', authMiddleware, todoController.renameChecklistItem)
